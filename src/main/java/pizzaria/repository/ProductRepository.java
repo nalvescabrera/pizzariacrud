414205447a71cfb6d2cb36ea708e461fb37c26d9
nalvescabrera <n.alvescabrera@gmail.com>
2025-01-08 23:32:26 -0300

    FEATURE/0.1: Criação de Service e Controller

--- src/main/java/pizzaria/repository/ProductRepository.java
+++ src/main/java/pizzaria/repository/ProductRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Product  extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<ProductRepository, Long> {
 
 
 
